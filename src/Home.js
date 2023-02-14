bf915246eab01bb10600b9fedbd458fb81093701
Alpana <alpana.nanda12@gmail.com>
2023-02-15 01:54:28 +0530

    fetch data from pdf

--- src/Home.js
+++ src/Home.js
@@ -1,26 +1,77 @@
-import React from "react";
+import React, { useEffect } from "react";
 import axios from "axios";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 const Home = () => {
-    
   let navigate = useNavigate();
-//   const [urlName, setUrlName] = useState("");
+  //   const [urlName, setUrlName] = useState("");
   const [url, setUrl] = useState("");
+  const [file, setFile] = useState("");
+
+  useEffect(() => {
+    console.log(file)
+  }, [file])
+  
+  // async function handleSubmit(event){
+  //   const response = await fetch(`http://localhost:8000/api/uploadPdf/`,{
+  //     method: "POST",
+  //    headers: {
+  //      "Content-Type": "multipart/form-data"
+  //     },
+      
+  //     body:JSON.stringify({
+  //       file:file,
+  //   })
+  //   })
+
+  //   let data = await response.json()
+  //   console.log(data)
+  // }
+  
+  function handleSubmit(event) {
+    event.preventDefault()
+    axios
+      .post("http://localhost:8000/api/uploadPdf/", 
+      {
+        file: file,
+      },
+      {
+        headers: {
+        'content-type': 'multipart/form-data',
+        
+        }
+      }
+      )
+      .then((resp) => {
+        console.log(resp.data);
+        localStorage.setItem("objectId", resp.data.objectId);
+        navigate("/sentences");
+      })
+      .catch((error) => {
+        console.error(error.response);
+      });
+
+  }
+
 
   useNavigate();
+
+  function handleFileChange(e){
+    let x = e.target.files[0]
+    setFile(x)
+  }
 
   async function callApi(e) {
     e.preventDefault();
     axios
       .post("http://localhost:8000/api/uploadUrl/", {
         // urlName: urlName,
-        url : url
+        url: url,
       })
       .then((resp) => {
         console.log(resp.data);
-        localStorage.setItem("objectId",resp.data.objectId)
-        navigate('/sentences')
+        localStorage.setItem("objectId", resp.data.objectId);
+        navigate("/sentences");
       })
       .catch((error) => {
         console.error(error.response);
@@ -32,7 +83,7 @@ const Home = () => {
       <div className="container mt-5">
         <form>
           <div class="form-group">
-          {/* <label for="websiteUrl">Enter Website Name</label>
+            {/* <label for="websiteUrl">Enter Website Name</label>
             <input
               type="url"
               class="form-control"
@@ -40,7 +91,7 @@ const Home = () => {
               placeholder="Enter Website Name"
               onChange={(e) => setUrlName(e.target.value)}
             /> */}
-            <br/>
+            <br />
             <label for="websiteUrl">Enter Website URL</label>
             <input
               type="url"
@@ -62,6 +113,14 @@ const Home = () => {
           >
             Submit
           </button>
+        </form>
+        <br/>
+        <form onSubmit={handleSubmit} enctype="multipart/form-data">
+        <label >Choose Pdf to upload</label><br/>
+          <input name="dinoPdf" id ="dinoPdf" type="file" onChange={(e) => handleFileChange(e)} /><br/>
+          <button type="submit" class="btn btn-primary mt-3" onClick={(e) => {
+              handleSubmit(e);
+            }}>Upload</button>
         </form>
       </div>
     </div>
